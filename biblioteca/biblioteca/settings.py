f47937f6958e05162c4f0a2990868d8edb6d823a
martimiranda <martimiranda2356@gmail.com>
2024-04-25 18:19:15 +0200

    last commit sprint server

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
