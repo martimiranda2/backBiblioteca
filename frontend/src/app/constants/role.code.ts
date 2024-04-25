2320616e69eff4e27af769357aec78805d4fa0e0
martimiranda <martimiranda2356@gmail.com>
2024-04-25 17:45:12 +0200

    elim-front

--- frontend/src/app/constants/role.code.ts
+++
@@ -1,6 +0,0 @@
-export enum Role {
-    ADMIN = 1,
-    PROFESSOR = 2,
-    ALUMNE = 3,
-    BIBLIO = 4,
-}
--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://127.0.0.1:8000',
-    uploadsUrl: 'http://localhost:4200/uploads',
-    socketUrl: 'http://127.0.0.1:8000',
-};
